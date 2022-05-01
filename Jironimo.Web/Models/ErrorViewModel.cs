1cc106cbb00187bbd5578f706b4fd9e083c70f40
Oleksandr Yesipov <oleksandr.yesipov@avanquest.com>
2022-05-01 15:49:30 +0300

    Add project files.

---
+++ Jironimo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jironimo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
