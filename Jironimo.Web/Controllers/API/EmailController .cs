373be291318147830989997d60688c022cd0fd36
Oleksandr Yesipov <oleksandr.yesipov@avanquest.com>
2022-06-06 22:28:36 +0300

    fix styles

--- Jironimo.Web/Controllers/API/EmailController .cs
+++ Jironimo.Web/Controllers/API/EmailController .cs
@@ -3,6 +3,7 @@ using Jironimo.Common.Models.Email;
 using Jironimo.Web.Helper;
 using Jironimo.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Jironimo.Web.Controllers.API
 {
@@ -12,17 +13,22 @@ namespace Jironimo.Web.Controllers.API
     {
         private readonly IRazorViewToStringRenderer _renderer;
         private readonly IMailService _mailService;
-        public EmailController(IMailService mailService, IRazorViewToStringRenderer renderer)
+        private readonly MailSettings _mailSettings;
+
+        public EmailController(IMailService mailService,
+                               IRazorViewToStringRenderer renderer,
+                               IOptions<MailSettings> mailSettings)
         {
             _mailService = mailService;
             _renderer = renderer;
+            _mailSettings = mailSettings.Value;
         }
 
         [HttpPost]
         public async Task<IActionResult> SendContactForm([FromForm] ContactFromViewModel request)
         {
             MailRequest mailRequest = new MailRequest();
-            mailRequest.ToEmail = request.Email;
+            mailRequest.ToEmail = _mailSettings.Mail;
             mailRequest.Subject = "Order";
             const string view = "~/Views/TemplatesEmails/ContactForm";
             var htmlBody = await _renderer.RenderViewToStringAsync($"{view}Html.cshtml", request);
