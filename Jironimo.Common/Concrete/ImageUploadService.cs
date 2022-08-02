7d1a93dbd3d4256ded8b8f1b50e98d8e6dce533c
ukraine\OYesipov <oleksandr.yesipov@avanquest.com>
2022-08-02 19:58:58 +0300

    fix routes

--- Jironimo.Common/Concrete/ImageUploadService.cs
+++ Jironimo.Common/Concrete/ImageUploadService.cs
@@ -1,11 +1,6 @@
 ﻿using Jironimo.Common.Abstract.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Jironimo.Common.Concrete
 {
